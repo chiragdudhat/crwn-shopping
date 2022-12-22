dfb486bc4a8db863de8a39effed4e47f58c59118
Chirag Dudhat <chiragdudhat332@gmail.com>
2022-12-22 11:03:25 -0500

    updating out directiry component and create categoried

---
+++ src/components/category-item/category-item.component.jsx
@@ -0,0 +1,19 @@
+import './category-item.component.scss';
+
+const CategoryItem = ({ category }) => {
+    const {imageUrl, title} =  category; 
+    
+    return(
+        <div className="category-container">
+            <div className='background-image' style={{
+                backgroundImage: `url(${imageUrl})`
+                }} />
+            <div className="category-body-container">
+                <h1>{title}</h1>
+                <p>Shop now</p>
+            </div>
+        </div>
+    )
+}
+
+export default CategoryItem
